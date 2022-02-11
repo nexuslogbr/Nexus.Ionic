2d881bafec4cf6ac714bbecf80ca2a8f715da25f
Diego Francisco <diego@cotexo.com.br>
2022-02-11 18:04:05 -0300

    Criação de componente de seleção superf. avaria

---
+++ src/pages/lancamento-avaria-selecao-superficie/lancamento-avaria-selecao-superficie.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NavController, NavParams } from 'ionic-angular';
+import * as $ from 'jquery';
+import { TipoAvaria } from '../../model/TipoAvaria';
+import { AvariaDataService } from '../../providers/avaria-data-service';
+import { GravidadeDataService } from '../../providers/gravidade-data-service';
+
+@Component({
+  selector: 'page-lancamento-avaria-selecao-superficie',
+  templateUrl: 'lancamento-avaria-selecao-superficie.html',
+})
+export class LancamentoAvariaSelecaoSuperficiePage {
+  title: string;
+  tiposAvaria: Array<TipoAvaria> = [];
+
+  formSelecaoSuperficie: FormGroup
+
+  formData = {
+    chassi: '',
+    modelo: '',
+    posicaoAtual: '',
+    cor: '',
+    status: '',
+    observacao: ''
+  };
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private avariaService: AvariaDataService,
+    private gravidadeService: GravidadeDataService,
+    private formBuilder: FormBuilder
+  ) {
+    this.title = 'Lançamento de Avaria';
+    this.formData = this.navParams.get('data');
+
+    this.formSelecaoSuperficie = formBuilder.group({
+      observacao: [this.formData.observacao],
+      chassi: [this.formData.chassi, Validators.required],
+      modelo: [this.formData.modelo, Validators.required],
+      tipoAvaria: ['', Validators.required]
+    });
+  }
+
+  ionViewDidEnter() {
+    this.loadTipoAvaria();
+  }
+
+  toggleMenu = function (this) {
+    $('.menu-body').toggleClass('show-menu');
+    $('menu-inner').toggleClass('show');
+    $('.icon-menu').toggleClass('close-menu');
+    $('side-menu').toggleClass('show');
+  };
+
+  loadTipoAvaria(){
+    this.avariaService.carregarTipoAvarias()
+    .subscribe(res => {
+      this.tiposAvaria = res.retorno;
+    })
+  }
+
+  loadPosicaoAvaria(){
+    // this.
+  }
+
+  loadGravidade(){
+    this.gravidadeService.carregarGravidades()
+    .subscribe(res => {
+      var result = res.retorno
+    })
+  }
+
+  onTipoAvariaChange(event){
+    this.formSelecaoSuperficie.patchValue({
+      tipoAvaria: event
+    });
+  }
+}
