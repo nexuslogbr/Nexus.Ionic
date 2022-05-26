fce9af132fb2d8345abad316ad57e2abe3a64223
Diego Francisco <deliasfrancisco@gmail.com>
2022-05-26 20:28:11 -0300

    Criação de metodo de consulta para popular o dashboard com as ultimas avarias

--- src/providers/avaria-data-service.ts
+++ src/providers/avaria-data-service.ts
@@ -7,6 +7,12 @@ const headers = new HttpHeaders({
   'Content-Type': 'application/json'
 });
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
 @Injectable()
 export class AvariaDataService{
   urlApi: string;
@@ -22,5 +28,12 @@ export class AvariaDataService{
   public carregarTipoAvarias() {
     let url = this.urlApi + '/tipoavaria/listartiposavaria?token=' + this.authService.getToken();
     return this.http.get<DataRetorno>(url, { headers: headers });
+  }
+
+  public carregarAvarias(model: any){
+    let dashboard = this.urlApi + "/lancamentoAvaria/Dashboard";
+
+    let url = this.urlApi + '/lancamentoAvaria/Dashboard';
+    return this.http.post<string>(url, model, httpOptions);
   }
 }
