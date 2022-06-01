1fae61b04712072ce3d74c3608421fd909e944ed
Daniel Ribeiro <danarrib@gmail.com>
2022-06-01 15:09:25 -0300

    Capacitor Stuff

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.patioautomotivo.app',
   appName: 'patio-automotivo-app',
   webDir: 'www',
   bundledWebRuntime: false,
