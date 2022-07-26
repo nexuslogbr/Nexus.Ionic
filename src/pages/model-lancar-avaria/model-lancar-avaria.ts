72ac3d615f6ac844a80bc74d55152b4e325c94e0
Diego Francisco <deliasfrancisco@gmail.com>
2022-07-26 17:42:20 -0300

    Alteração de modal para exibir status da vistoria

--- src/pages/model-lancar-avaria/model-lancar-avaria.ts
+++ src/pages/model-lancar-avaria/model-lancar-avaria.ts
@@ -1,63 +1,103 @@
-import { Component, OnInit } from '@angular/core';
-import { Modal, ModalController, NavController, NavParams, ViewController } from 'ionic-angular';
-import { Momento } from '../../model/Momento';
+import { Component } from '@angular/core';
+import { NavController, NavParams, ViewController, Modal, ModalController } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service/auth-service';
+import { ModalErrorComponent } from '../../components/modal-error/modal-error';
+import * as $ from 'jquery';
 import { Veiculo } from '../../model/veiculo';
+import { Momento } from '../../model/Momento';
 import { LancamentoAvariaSelecaoSuperficiePage } from '../lancamento-avaria-selecao-superficie/lancamento-avaria-selecao-superficie';
+import { LancamentoAvariaVistoriaLancarPage } from '../lancamento-avaria-vistoria-lancar/lancamento-avaria-vistoria-lancar';
+import { Arquivo } from '../../model/arquivo';
+import { Navio } from '../../model/navio';
 
 @Component({
   selector: 'page-model-lancar-avaria',
   templateUrl: 'model-lancar-avaria.html',
 })
-export class ModelLancarAvariaPage implements OnInit {
-
-  public primaryMessage: string;
-  public secundaryMessage: string;
-  public onConfirm?: Function;
-  public onCancel?: Function;
+export class ModelLancarAvariaPage {
 
   formData = {
     veiculo: new Veiculo(),
-    momento: new Momento()
+    momento: new Momento(),
+    arquivo: new Arquivo(),
+    navio: new Navio(),
+    veiculos: new Array<Veiculo>()
   };
 
+  primaryColor: string;
+  secondaryColor: string;
+  inputColor: string;
+  buttonColor: string;
+
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
     private modal: ModalController,
-    public viewCtrl: ViewController
+    private navParam: NavParams,
+    private view: ViewController,
+    public navCtrl: NavController,
+    public authService: AuthService
   ) {
-    this.primaryMessage = 'Chassi vistoriado';
-    this.secundaryMessage = 'Lançar avaria?';
-
-    this.formData = this.navParams.get('data');
+    if (localStorage.getItem('tema') == "Cinza" || !localStorage.getItem('tema')) {
+      this.primaryColor = '#595959';
+      this.secondaryColor = '#484848';
+      this.inputColor = '#595959';
+      this.buttonColor = "#595959";
+    } else {
+      this.primaryColor = '#06273f';
+      this.secondaryColor = '#00141b';
+      this.inputColor = '#06273f';
+      this.buttonColor = "#1c6381";
+    }
   }
 
-  ngOnInit(): void {
-    // if (this.onConfirm == null && this.onCancel == null) {
-    //   setTimeout(() => {
-    //     this.viewCtrl.dismiss();
-    //   }, 1000);
-    // }
+  ionViewWillLoad() {
+    const data = this.navParam.get('data');
+    this.formData = data;
   }
 
-  onConfirmAvariaClick() {
-    this.viewCtrl.dismiss();
+  closeModal() {
+    const modal: Modal = this.modal.create(LancamentoAvariaVistoriaLancarPage, {
+      arquivo: this.formData.arquivo,
+    });
 
+    modal.present();
+    const data = {
+      name: 'Hingo',
+      cargo: 'Front',
+    };
+    this.view.dismiss(data);
+  }
+
+  toggleMenu = function (this) {
+    $('.menu-body').toggleClass('show-menu');
+    $('menu-inner').toggleClass('show');
+    $('.icon-menu').toggleClass('close-menu');
+    $('side-menu').toggleClass('show');
+  };
+
+  openPage() {
     const modal: Modal = this.modal.create(LancamentoAvariaSelecaoSuperficiePage, {
       data: this.formData,
     });
     modal.present();
 
+
+    modal.onDidDismiss((data) => {
+      console.log(data);
+    });
+    modal.onWillDismiss((data) => {
+      console.log(data);
+    });
   }
 
-  onCancelClick() {
-    this.viewCtrl.dismiss();
-    this.onCancel();
-  }
+  openModalErro(data) {
+    const chassiModal: Modal = this.modal.create(ModalErrorComponent, {
+      data: data,
+    });
+    chassiModal.present();
 
-  dismiss() {
-    if (!this.onCancel && !this.onConfirm) {
-      this.viewCtrl.dismiss();
-    }
+    chassiModal.onDidDismiss((data) => {
+      console.log(data);
+    });
+    chassiModal.onWillDismiss((data) => {});
   }
 }
