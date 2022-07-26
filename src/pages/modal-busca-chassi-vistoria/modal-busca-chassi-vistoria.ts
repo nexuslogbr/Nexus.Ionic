c7f510cbfe5b38f6aefc81d5e302dd42fa905c9a
Diego Francisco <deliasfrancisco@gmail.com>
2022-07-26 17:43:04 -0300

    Melhoria na busca de chassis e menu superior de retorno

--- src/pages/modal-busca-chassi-vistoria/modal-busca-chassi-vistoria.ts
+++ src/pages/modal-busca-chassi-vistoria/modal-busca-chassi-vistoria.ts
@@ -13,8 +13,9 @@ import { finalize } from 'rxjs/operators';
 import { VistoriaDataService } from '../../providers/vistoria-service';
 import { DataRetorno } from '../../model/dataretorno';
 import { AlertService } from '../../providers/alert-service';
-import { LancamentoAvariaVistoriaLancarPage } from '../lancamento-avaria-vistoria-lancar/lancamento-avaria-vistoria-lancar';
 import { ModelLancarAvariaPage } from '../model-lancar-avaria/model-lancar-avaria';
+import { Arquivo } from '../../model/arquivo';
+import { Navio } from '../../model/navio';
 
 @Component({
   selector: 'page-modal-busca-chassi-vistoria',
@@ -26,19 +27,25 @@ export class ModalBuscaChassiVistoriaPage {
   options: BarcodeScannerOptions;
   token: string;
   chassi: string;
-  responseData: any;
   url: string;
   qrCodeText: string;
   modoOperacao: number;
   formData = {
-    posicaoAtual: '',
     veiculo: new Veiculo(),
-    momento: new Momento()
+    momento: new Momento(),
+    arquivo: new Arquivo(),
+    navio: new Navio(),
+    veiculos: new Array<Veiculo>()
   };
 
   @ViewChild('chassiInput') chassiInput;
   formControlChassi = new FormControl('');
   modalChassi: Modal
+
+  primaryColor: string;
+  secondaryColor: string;
+  inputColor: string;
+  buttonColor: string;
 
   constructor(
     private http: HttpClient,
@@ -58,6 +65,19 @@ export class ModalBuscaChassiVistoriaPage {
 
     this.formData = this.navParams.get('data');
 
+    if (localStorage.getItem('tema') == "Cinza" || !localStorage.getItem('tema')) {
+      this.primaryColor = '#595959';
+      this.secondaryColor = '#484848';
+      this.inputColor = '#595959';
+      this.buttonColor = "#595959";
+    }
+    else {
+      this.primaryColor = '#06273f';
+      this.secondaryColor = '#00141b';
+      this.inputColor = '#06273f';
+      this.buttonColor = "#1c6381";
+    }
+
     this.formControlChassi.valueChanges.debounceTime(500).subscribe((value) => {
       if (value && value.length) {
         {
@@ -65,8 +85,7 @@ export class ModalBuscaChassiVistoriaPage {
             let chassi = value.replace(/[\W_]+/g, '');
             setTimeout(() => {
               this.buscarChassi(chassi);
-              this.formData.veiculo.chassi = '';
-              this.chassi = chassi;
+              this.chassi = '';
             }, 500);
           }
         }
@@ -86,7 +105,7 @@ export class ModalBuscaChassiVistoriaPage {
       setTimeout(() => {
         this.chassiInput.setFocus();
       }, 1000);
-    this.formData.veiculo.chassi = '';
+    this.chassi = '';
   }
 
   scan() {
@@ -96,50 +115,33 @@ export class ModalBuscaChassiVistoriaPage {
       resultDisplayDuration: 0,
     };
 
-    this.authService.showLoading();
     this.barcodeScanner.scan(this.options).then((barcodeData) => {
         this.qrCodeText = barcodeData.text;
         if (this.qrCodeText && this.qrCodeText.length) {
           let partChassi = this.qrCodeText.substr(this.qrCodeText.length - 17, 17);
-          this.formData.veiculo.chassi = partChassi;
           this.buscarChassi(partChassi);
         }
       },
       (err) => {
-        this.authService.hideLoading();
         let data = 'Erro de qr code!';
-        this.openModalErro(data, true);
+        this.openModalErro(data);
       }
     );
   }
 
-  buscarChassi(partChassi) {
-    let uriBuscaChassi = '/veiculos/ConsultarChassi?token=' + this.authService.getToken() + '&chassi=' + partChassi;
+  buscarChassi(chassi) {
     this.authService.showLoading();
 
-    this.http.get(this.url + uriBuscaChassi)
-    .subscribe(
-      (res) => {
-        this.responseData = res;
-        if (this.responseData.sucesso) {
-          this.vistoriarChassi(this.responseData.retorno);
-        }
-        else {
-          if (this.modoOperacao == 1 || partChassi.length < 17) {
-            this.openModalErro(this.responseData.mensagem, true);
-          }
-          else if (this.responseData.dataErro == 'CHASSI_ALREADY_RECEIVED') {
-            this.openModalErro(this.responseData.mensagem, true);
-          }
-          else {
-           this.openModalErro(this.responseData.mensagem, true);
-          }
-        }
-      },
-      (error) => {
-        this.openModalErro(error.status + ' - ' + error.statusText, true);
-      }
-    );
+    let veiculo = this.formData.veiculos.filter(x => x.chassi == chassi).map(x => x)[0];
+
+    if (veiculo && chassi.length == 17) {
+      this.formData.veiculo = veiculo;
+      this.vistoriarChassi(veiculo)
+    }
+    else {
+      this.alertService.showError('CHASSI INVÀLIDO');
+      this.authService.hideLoading();
+    }
   }
 
   vistoriarChassi(veiculo: Veiculo){
@@ -151,17 +153,14 @@ export class ModalBuscaChassiVistoriaPage {
     )
     .subscribe((res:DataRetorno) => {
       if (res.sucesso) {
-
-        this.formData.veiculo.chassi = this.chassi;
-        this.view.dismiss();
-
         const modal: Modal = this.modal.create(ModelLancarAvariaPage, {
           data: this.formData,
         });
         modal.present();
-      }
-      else if (!res.sucesso) {
+
         this.view.dismiss();
+      }
+      else {
         this.alertService.showError(res.mensagem);
       }
     });
@@ -178,7 +177,7 @@ export class ModalBuscaChassiVistoriaPage {
     $('side-menu').toggleClass('show');
   };
 
-  openModalErro(data, byScanner: boolean) {
+  openModalErro(data) {
     const chassiModal: Modal = this.modal.create(ModalErrorComponent, {
       data: data,
     });
