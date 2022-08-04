a2d5acfdd33c58012a881e37300404963d7925fc
Diego Francisco <deliasfrancisco@gmail.com>
2022-08-04 18:45:15 -0300

    Alteração da referencia da entidade

--- src/model/navio.ts
+++ src/model/navio.ts
@@ -16,6 +16,6 @@ export class Navio {
   public statusConfiguracao: string;
   public statusData: string;
   public conferenciaStatus: number;
-  public statusArquivo: string;
+  public statusVistoria: string;
   public momentoId?: number;
 }
